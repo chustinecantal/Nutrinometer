08ba38e59cc48feb97d0d7340ff94e7e8238d472
stephanieperez <123723582+Stephanie017@users.noreply.github.com>
2024-04-21 22:24:55 +0800

    Add files via upload

---
+++ Nutrinometer/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Nutrinometer
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection(@" Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\stephanie perez\OneDrive\Documents\clientUser.mdf;Integrated Security = True; Connect Timeout = 30");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_showPass_CheckedChanged(object sender, EventArgs e)
+        {
+            login_password.PasswordChar = login_showPass.Checked ? '\0' : '*';
+        }
+
+        private void login_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            if(login_username.Text == ""
+                || login_password.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields"
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if(connect.State == ConnectionState.Closed)
+                {
+                    try
+                    {
+                        connect.Open();
+
+                        string selectData = "SELECT * FROM client WHERE username = @username";
+
+                        using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                        {
+                            cmd.Parameters.AddWithValue("@username", login_username.Text.Trim());
+
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                            DataTable table = new DataTable();
+                            adapter.Fill(table);
+
+                            if (table.Rows.Count == 0)
+                            {
+                                MessageBox.Show("You don't have an account. Please register or sign up.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                string storedPassword = table.Rows[0]["password"].ToString();
+
+                                if (storedPassword != login_password.Text.Trim())
+                                {
+                                    MessageBox.Show("Incorrect Password", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Login successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+
+    }
+
+        private void login_signupBtn_Click(object sender, EventArgs e)
+        {
+            RegisterForm regForm = new RegisterForm();
+            regForm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Nutrinometer/RegisterForm.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+
+namespace Nutrinometer
+{
+    public partial class RegisterForm : Form
+    {
+        SqlConnection connect = new SqlConnection(@" Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\stephanie perez\OneDrive\Documents\clientUser.mdf;Integrated Security = True; Connect Timeout = 30");
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void signup_loginBtn_Click(object sender, EventArgs e)
+        {
+            Form1 loginForm = new Form1();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void signup_showPass_CheckedChanged(object sender, EventArgs e)
+        {
+            signup_password.PasswordChar = signup_showPass.Checked ? '\0' : '*';
+        }
+
+        private void signup_btn_Click(object sender, EventArgs e)
+        {
+            if (signup_username.Text == "" || signup_password.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!PasswordMeetsRequirements(signup_password.Text))
+            {
+                MessageBox.Show("Password must contain at least one uppercase letter, one lowercase letter, and one number", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    if (connect.State != ConnectionState.Open)
+                    {
+                        connect.Open();
+
+                        string selectUsername = "SELECT COUNT(Person_No) FROM client WHERE username = @user";
+
+                        using (SqlCommand checkUser = new SqlCommand(selectUsername, connect))
+                        {
+                            checkUser.Parameters.AddWithValue("@user", signup_username.Text.Trim());
+                            int count = (int)checkUser.ExecuteScalar();
+
+                            if (count > 0)
+                            {
+                                MessageBox.Show(signup_username.Text.Trim() + " is already taken", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+                        }
+
+                        DateTime today = DateTime.Today;
+
+                        string insertData = "INSERT INTO client (username, password, date_register) VALUES (@username, @password, @dateReg)";
+
+                        using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                        {
+                            cmd.Parameters.AddWithValue("@username", signup_username.Text.Trim());
+                            cmd.Parameters.AddWithValue("@password", signup_password.Text.Trim());
+                            cmd.Parameters.AddWithValue("@dateReg", today);
+
+                            cmd.ExecuteNonQuery();
+
+                            MessageBox.Show("Registered successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        private bool PasswordMeetsRequirements(string password)
+        {
+            // Password must contain at least one uppercase letter, one lowercase letter, and one number
+            return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit);
+        }
+
+
+    }
+}
