8b2ab90e8f2176c2a5e82f139a0c3a3c99786f5d
CHUSTINE MAE CANTAL <129162395+chustinecantal@users.noreply.github.com>
2024-05-20 10:19:09 +0800

    Add files via upload

---
+++ Nutrinometer/CsvExporter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace Nutrinometer
+{
+    internal class CsvExporter
+    {
+        private MySqlConnection connection;
+
+        public CsvExporter(MySqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void ExportTableToCsv(string tableName, string fileName)
+        {
+            try
+            {
+                // Create SQL command to select all data from the specified table
+                string query = $"SELECT * FROM {tableName}";
+                MySqlCommand command = new MySqlCommand(query, connection);
+
+                // Create a DataReader to fetch data from the database
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    // Create a StreamWriter to write data to a CSV file
+                    using (StreamWriter writer = new StreamWriter(fileName))
+                    {
+                        // Write column headers to the CSV file
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            writer.Write(reader.GetName(i));
+                            if (i < reader.FieldCount - 1)
+                                writer.Write(",");
+                            else
+                                writer.WriteLine();
+                        }
+
+                        // Write data rows to the CSV file
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                if (!reader.IsDBNull(i))
+                                {
+                                    writer.Write(reader[i].ToString());
+                                }
+                                if (i < reader.FieldCount - 1)
+                                    writer.Write(",");
+                                else
+                                    writer.WriteLine();
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Table '{tableName}' exported to '{fileName}' successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error exporting table '{tableName}' to CSV: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Nutrinometer/UserManagement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Nutrinometer
+{
+    internal class UserManagement
+    {
+        private string connectionString; // Connection string for database access
+        private MySqlConnection connection;
+
+        public UserManagement(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public bool RegisterUser(string firstName, string lastName, string username, double weight, int age, char sex, double height, int activityLevel)
+        {
+            try
+            {
+                connection.Open();
+                // SQL command to insert user data into the userinfo table
+                string query = "INSERT INTO userinfo (username, firstname, lastname, weight, age, sex, height, activity_level) " +
+                               "VALUES (@Username, @FirstName, @LastName, @Weight, @Age, @Sex, @Height, @ActivityLevel)";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                command.Parameters.AddWithValue("@Weight", weight);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Sex", sex);
+                command.Parameters.AddWithValue("@Height", height);
+                command.Parameters.AddWithValue("@ActivityLevel", activityLevel);
+                command.ExecuteNonQuery();
+
+                // Inform the user about their initial password (userId)
+                int userId = (int)command.LastInsertedId; // Get the auto-generated userId
+                MessageBox.Show($"User registered successfully. Your userId (initial password) is: {userId}");
+
+                return true; // Registration successful
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR!" + ex.Message);
+                return false; // Registration failed
+            }
+        }
+
+
+
+
+    }
+}
